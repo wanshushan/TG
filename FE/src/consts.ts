29a2c458e9e3d45232006cfc39c1024e92f001d8
wanshushan <3445497806@qq.com>
2026-03-17 20:38:52 +0800

    更改了页脚图标，现在在config.ts中配置即可

--- FE/src/consts.ts
+++ FE/src/consts.ts
@@ -3,3 +3,33 @@
 
 export const SITE_TITLE = '灵·舌';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+
+//  TODO: 目前这些常量分散在各个文件中，考虑集中管理。
+
+
+// 页脚图标以及链接
+
+export type FooterSocialLink = {
+    name: string;
+    href: string;
+    icon: `fa6-brands:${string}`;
+};
+
+export const FOOTER_SOCIAL_LINKS: FooterSocialLink[] = [
+    {
+        name: "GitHub",
+        href: "https://github.com/wanshushan",
+        icon: "fa6-brands:github"
+    },
+    {
+        name: "QQ",
+        href: "https://qq.com",
+        icon: "fa6-brands:qq"
+    },
+    {
+        name: "B站",
+        href: "https://bilibili.com",
+        icon: "fa6-brands:bilibili"
+    }
+];
---
+++ Readme.md
@@ -0,0 +1,7 @@
+![alt text](pjshare1.png)
+
+# 灵·舌 
+
+## 前端基于 astro 构建
+
+## 后端还不知道
